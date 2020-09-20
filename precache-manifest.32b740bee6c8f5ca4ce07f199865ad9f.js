aa778846b390fd37c076bf176b1401a995a15f4b
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-09-20 15:30:24 +0530

    Updates

---
+++ precache-manifest.32b740bee6c8f5ca4ce07f199865ad9f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba53b0f19a9a45be4a6e1629a16ad85",
+    "url": "/neki-test/index.html"
+  },
+  {
+    "revision": "28758e92ff4747408b9d",
+    "url": "/neki-test/static/css/main.b5410eb4.chunk.css"
+  },
+  {
+    "revision": "e9db1cd0e249722511c6",
+    "url": "/neki-test/static/js/2.130877ca.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/neki-test/static/js/2.130877ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28758e92ff4747408b9d",
+    "url": "/neki-test/static/js/main.cbdad7f6.chunk.js"
+  },
+  {
+    "revision": "935b6a47fe64f52ca53a",
+    "url": "/neki-test/static/js/runtime-main.0d726540.js"
+  },
+  {
+    "revision": "3644ee708bf9efe747ea78b2965ca58f",
+    "url": "/neki-test/static/media/bg.3644ee70.jpg"
+  },
+  {
+    "revision": "d40c6d698e55c440028eaac7eff3a7c5",
+    "url": "/neki-test/static/media/donate.d40c6d69.jpg"
+  },
+  {
+    "revision": "6ce02f8e1f9d2fa5aa6340b662404d29",
+    "url": "/neki-test/static/media/education.6ce02f8e.jpg"
+  },
+  {
+    "revision": "d39cefb4fecd31c2f4077a0c2fb8cb04",
+    "url": "/neki-test/static/media/food.d39cefb4.jpg"
+  },
+  {
+    "revision": "04350b51b95651facc49f31b07a8415a",
+    "url": "/neki-test/static/media/people.04350b51.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/neki-test/precache-manifest.32b740bee6c8f5ca4ce07f199865ad9f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/neki-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
